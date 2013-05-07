b9534f66ddf22a4370dd0ee180391f6dafa7f2e5
kyleprager <kyleprager@gmail.com>
2013-05-07 11:03:43 -0700

    parseFloat instead of parseInt to keep granularity of distances for routes.

--- traffic.js
+++ traffic.js
@@ -2,9 +2,11 @@ $ = require('jquery');
 
 (function () {
     var results = [];
-    var urls = ['https://maps.google.com/maps?saddr=Auzerais+Ave,+San+Jose,+CA&daddr=37.294157,-121.9698917+to:1601+S+De+Anza+Blvd+%23165,+Cupertino,+CA&hl=en&sll=37.297681,-121.980171&sspn=0.095863,0.158787&geocode=Fel8OQId5-27-CkVGhHIV8uPgDE4sEctF9WxdA%3BFU0QOQIdHeO6-CmbG3l7NTWOgDGdLBv0IhBeGA%3BFfcVOQId2u25-CmV5Mb3IbWPgDHSYGaXGvYtxw&mra=dpe&mrsp=1&sz=13&via=1&t=m&z=13',
+    var urls = [
+        'https://maps.google.com/maps?saddr=Auzerais+Ave,+San+Jose,+CA&daddr=37.294157,-121.9698917+to:1601+S+De+Anza+Blvd+%23165,+Cupertino,+CA&hl=en&sll=37.297681,-121.980171&sspn=0.095863,0.158787&geocode=Fel8OQId5-27-CkVGhHIV8uPgDE4sEctF9WxdA%3BFU0QOQIdHeO6-CmbG3l7NTWOgDGdLBv0IhBeGA%3BFfcVOQId2u25-CmV5Mb3IbWPgDHSYGaXGvYtxw&mra=dpe&mrsp=1&sz=13&via=1&t=m&z=13',
         'https://maps.google.com/maps?saddr=Auzerais+Ave,+San+Jose,+CA&daddr=1601+S+De+Anza+Blvd+%23165,+Cupertino,+CA&hl=en&ll=37.315021,-121.978455&spn=0.09584,0.158787&sll=37.306576,-121.965717&sspn=0.095851,0.158787&geocode=Fel8OQId5-27-CkVGhHIV8uPgDE4sEctF9WxdA%3BFfcVOQId2u25-CmV5Mb3IbWPgDHSYGaXGvYtxw&mra=ls&t=m&z=13',
-        'https://maps.google.com/maps?saddr=Auzerais+Ave,+San+Jose,+CA&daddr=37.29537,-121.931773+to:37.2941684,-121.9690175+to:1601+S+De+Anza+Blvd+%23165,+Cupertino,+CA&hl=en&sll=37.283682,-121.970387&sspn=0.04794,0.079393&geocode=Fel8OQId5-27-CkVGhHIV8uPgDE4sEctF9WxdA%3BFQoVOQIdA3i7-Ckpr5a3xTSOgDH_UanUSeqAgg%3BFVgQOQIdh-a6-CmbG3l7NTWOgDGdLBv0IhBeGA%3BFfcVOQId2u25-CmV5Mb3IbWPgDHSYGaXGvYtxw&mra=dpe&mrsp=2&sz=14&via=1,2&t=m&z=14'
+        'https://maps.google.com/maps?saddr=Auzerais+Ave,+San+Jose,+CA&daddr=37.29537,-121.931773+to:37.2941684,-121.9690175+to:1601+S+De+Anza+Blvd+%23165,+Cupertino,+CA&hl=en&sll=37.283682,-121.970387&sspn=0.04794,0.079393&geocode=Fel8OQId5-27-CkVGhHIV8uPgDE4sEctF9WxdA%3BFQoVOQIdA3i7-Ckpr5a3xTSOgDH_UanUSeqAgg%3BFVgQOQIdh-a6-CmbG3l7NTWOgDGdLBv0IhBeGA%3BFfcVOQId2u25-CmV5Mb3IbWPgDHSYGaXGvYtxw&mra=dpe&mrsp=2&sz=14&via=1,2&t=m&z=14',
+        'https://maps.google.com/maps?saddr=Auzerais+Ave,+San+Jose,+CA&daddr=37.3234712,-121.9278006+to:37.3231072,-121.9866874+to:37.3229887,-122.0321463+to:1601+S+De+Anza+Blvd+%23165,+Cupertino,+CA&hl=en&ll=37.299593,-121.983089&spn=0.083843,0.145741&sll=37.30246,-121.994934&sspn=0.08384,0.145741&geocode=Fel8OQId5-27-CkVGhHIV8uPgDE4sEctF9WxdA%3BFc-COQIdiIe7-CnJl0QqP8uPgDFLmCqth_i6ig%3BFWOBOQIdgaG6-Cl1jp9chMqPgDHUQgytrDtpVw%3BFeyAOQId7u-5-CkpZMuJrrWPgDFfQ4IFo7NOVw%3BFfcVOQId2u25-CmV5Mb3IbWPgDHSYGaXGvYtxw&mra=dpe&mrsp=3&sz=13&via=1,2,3&t=m&z=13'
     ];
 
     var deferredList = [];
@@ -69,6 +71,6 @@ $ = require('jquery');
     }
 
     function splitTimeDistanceStrings(str) {
-        return parseInt(str.split(' ')[0]);
+        return parseFloat(str.split(' ')[0]);
     }
 })();
